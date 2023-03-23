53c731c5d075aecd1eb813d6432554466c3d7f96
Nicolas Giron <nicogiron09@gmail.com>
2023-03-23 15:44:43 -0300

    Se sube Practica 6

---
+++ labNetPractica6/labnetpractic6/Lab.EF.UI/Lab.EF.UI/FrmSegundaParte.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Lab.EF.UI
+{
+    partial class FrmSegundaParte
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnquery7 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnquery8 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.btnquery9 = new System.Windows.Forms.Button();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.btnquery10 = new System.Windows.Forms.Button();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.btnquery11 = new System.Windows.Forms.Button();
+            this.groupBox7 = new System.Windows.Forms.GroupBox();
+            this.btnquery12 = new System.Windows.Forms.Button();
+            this.groupBox8 = new System.Windows.Forms.GroupBox();
+            this.btnquery13 = new System.Windows.Forms.Button();
+            this.grilladatos = new System.Windows.Forms.DataGridView();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.groupBox7.SuspendLayout();
+            this.groupBox8.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grilladatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.grilladatos);
+            this.groupBox2.Location = new System.Drawing.Point(12, 87);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(828, 258);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Lista";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnquery7);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(96, 69);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Punto7";
+            // 
+            // btnquery7
+            // 
+            this.btnquery7.Location = new System.Drawing.Point(6, 19);
+            this.btnquery7.Name = "btnquery7";
+            this.btnquery7.Size = new System.Drawing.Size(74, 36);
+            this.btnquery7.TabIndex = 0;
+            this.btnquery7.Text = "Query 7";
+            this.btnquery7.UseVisualStyleBackColor = true;
+            this.btnquery7.Click += new System.EventHandler(this.btnquery7_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnquery8);
+            this.groupBox3.Location = new System.Drawing.Point(114, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(96, 69);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Punto8";
+            // 
+            // btnquery8
+            // 
+            this.btnquery8.Location = new System.Drawing.Point(6, 19);
+            this.btnquery8.Name = "btnquery8";
+            this.btnquery8.Size = new System.Drawing.Size(74, 36);
+            this.btnquery8.TabIndex = 0;
+            this.btnquery8.Text = "Query 8";
+            this.btnquery8.UseVisualStyleBackColor = true;
+            this.btnquery8.Click += new System.EventHandler(this.btnquery8_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.btnquery9);
+            this.groupBox4.Location = new System.Drawing.Point(216, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(96, 69);
+            this.groupBox4.TabIndex = 10;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Punto9";
+            // 
+            // btnquery9
+            // 
+            this.btnquery9.Location = new System.Drawing.Point(6, 19);
+            this.btnquery9.Name = "btnquery9";
+            this.btnquery9.Size = new System.Drawing.Size(74, 36);
+            this.btnquery9.TabIndex = 0;
+            this.btnquery9.Text = "Query 9";
+            this.btnquery9.UseVisualStyleBackColor = true;
+            this.btnquery9.Click += new System.EventHandler(this.btnquery9_Click);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.btnquery10);
+            this.groupBox5.Location = new System.Drawing.Point(318, 12);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(96, 69);
+            this.groupBox5.TabIndex = 11;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Punto10";
+            // 
+            // btnquery10
+            // 
+            this.btnquery10.Location = new System.Drawing.Point(6, 19);
+            this.btnquery10.Name = "btnquery10";
+            this.btnquery10.Size = new System.Drawing.Size(74, 36);
+            this.btnquery10.TabIndex = 0;
+            this.btnquery10.Text = "Query 10";
+            this.btnquery10.UseVisualStyleBackColor = true;
+            this.btnquery10.Click += new System.EventHandler(this.btnquery10_Click);
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.btnquery11);
+            this.groupBox6.Location = new System.Drawing.Point(420, 12);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(96, 69);
+            this.groupBox6.TabIndex = 12;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Punto11";
+            // 
+            // btnquery11
+            // 
+            this.btnquery11.Location = new System.Drawing.Point(6, 19);
+            this.btnquery11.Name = "btnquery11";
+            this.btnquery11.Size = new System.Drawing.Size(74, 36);
+            this.btnquery11.TabIndex = 0;
+            this.btnquery11.Text = "Query 11";
+            this.btnquery11.UseVisualStyleBackColor = true;
+            this.btnquery11.Click += new System.EventHandler(this.btnquery11_Click);
+            // 
+            // groupBox7
+            // 
+            this.groupBox7.Controls.Add(this.btnquery12);
+            this.groupBox7.Location = new System.Drawing.Point(522, 12);
+            this.groupBox7.Name = "groupBox7";
+            this.groupBox7.Size = new System.Drawing.Size(96, 69);
+            this.groupBox7.TabIndex = 13;
+            this.groupBox7.TabStop = false;
+            this.groupBox7.Text = "Punto12";
+            // 
+            // btnquery12
+            // 
+            this.btnquery12.Location = new System.Drawing.Point(6, 19);
+            this.btnquery12.Name = "btnquery12";
+            this.btnquery12.Size = new System.Drawing.Size(74, 36);
+            this.btnquery12.TabIndex = 0;
+            this.btnquery12.Text = "Query 12";
+            this.btnquery12.UseVisualStyleBackColor = true;
+            this.btnquery12.Click += new System.EventHandler(this.btnquery12_Click);
+            // 
+            // groupBox8
+            // 
+            this.groupBox8.Controls.Add(this.btnquery13);
+            this.groupBox8.Location = new System.Drawing.Point(624, 12);
+            this.groupBox8.Name = "groupBox8";
+            this.groupBox8.Size = new System.Drawing.Size(96, 69);
+            this.groupBox8.TabIndex = 13;
+            this.groupBox8.TabStop = false;
+            this.groupBox8.Text = "Punto13";
+            // 
+            // btnquery13
+            // 
+            this.btnquery13.Location = new System.Drawing.Point(6, 19);
+            this.btnquery13.Name = "btnquery13";
+            this.btnquery13.Size = new System.Drawing.Size(74, 36);
+            this.btnquery13.TabIndex = 0;
+            this.btnquery13.Text = "Query 13";
+            this.btnquery13.UseVisualStyleBackColor = true;
+            this.btnquery13.Click += new System.EventHandler(this.btnquery13_Click);
+            // 
+            // grilladatos
+            // 
+            this.grilladatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grilladatos.Location = new System.Drawing.Point(6, 19);
+            this.grilladatos.Name = "grilladatos";
+            this.grilladatos.ReadOnly = true;
+            this.grilladatos.Size = new System.Drawing.Size(816, 233);
+            this.grilladatos.TabIndex = 0;
+            // 
+            // FrmSegundaParte
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(851, 357);
+            this.Controls.Add(this.groupBox8);
+            this.Controls.Add(this.groupBox7);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FrmSegundaParte";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Consultas 7-13";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox7.ResumeLayout(false);
+            this.groupBox8.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grilladatos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnquery7;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnquery8;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button btnquery9;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Button btnquery10;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.Button btnquery11;
+        private System.Windows.Forms.GroupBox groupBox7;
+        private System.Windows.Forms.Button btnquery12;
+        private System.Windows.Forms.GroupBox groupBox8;
+        private System.Windows.Forms.Button btnquery13;
+        private System.Windows.Forms.DataGridView grilladatos;
+    }
+}
---
+++ labNetPractica6/labnetpractic6/Lab.EF.UI/Lab.MVC/Models/ShippersView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab.MVC.Models
+{
+    public class ShippersView
+    {
+        public int ShippersID { get; set; }
+        public string CompanyName { get; set; }
+        public string Phone { get; set; }
+    }
+}
